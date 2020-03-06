de30b834e4a79309f488c3d23d9afbb8f83cfa09
Jonathan Morocho <jmorocho35@gmail.com>
2020-03-06 14:35:23 -0500

    terminado

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -15,24 +15,50 @@ export class ProductosService {
     this.cargarProductos();
   }
 
-  private cargarProductos(){
-    this.http.get('https://angula-html-f9bf4.firebaseio.com/productos_idx.json')
-    .subscribe((res: Producto[]) => {
-      //console.log(res)
-      this.cargando = false;
-      this.productos = res
+  private cargarProductos() {
+    return new Promise((resolve, reject) => {
+      this.http.get('https://angula-html-f9bf4.firebaseio.com/productos_idx.json')
+        .subscribe((res: Producto[]) => {
+          //console.log(res)
+          this.cargando = false;
+          this.productos = res;
+          resolve();
+        });
     });
+
   }
 
-  public getProducto(id: string){
+  public getProducto(id: string) {
     return this.http
-               .get(`https://angula-html-f9bf4.firebaseio.com/productos/${id}.json`);
+      .get(`https://angula-html-f9bf4.firebaseio.com/productos/${id}.json`);
   }
 
-  buscarProducto(termino: string){
-    this.productosFiltrado = this.productos.filter( producto => {
-      return true;
-    });
-    console.log(this.productosFiltrado)
+  buscarProducto(termino: string) {
+
+    if(this.productos.length === 0){
+      //carga productos
+      this.cargarProductos().then(()=>{
+        //ejecuta despues de tener los productos
+        //Aplicar filtro
+        this.filtrarProductos(termino)
+      });
+    } else{
+      //Aplico el filtro
+      this.filtrarProductos(termino)
+    }
+
+    
+  }
+
+  private filtrarProductos(termino: string){
+      console.log(this.productos)
+      this.productosFiltrado = []
+      termino = termino.toLocaleLowerCase();
+      this.productos.forEach(prod => {
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+        if(prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >=0){
+          this.productosFiltrado.push(prod);
+        }
+      });
   }
 }
